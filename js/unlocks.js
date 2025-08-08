7811df209ab08ccf6fa242cef288a6f8de18a372
Ethan <ethanbernard25@gmail.com>
2025-08-09 00:01:16 +0200

    unlocked day 4

--- js/unlocks.js
+++ js/unlocks.js
@@ -2,4 +2,4 @@
 // Manually edit this list to control which days are accessible
 
 // window.unlockedDays = [1,2,3,4,5,6,7,8,,9,10,11,12,13,14,15,16,17,18]; // Update this list to unlock more days (e.g., [1, 2, 3, 4])
-window.unlockedDays = [1,2,3];
+window.unlockedDays = [1,2,3,4];
